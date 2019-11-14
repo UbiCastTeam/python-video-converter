2a04938a8efdb9e5b3183965b0226a9c62690060
Geoffrey Guéret <geoffrey.gueret@ubicast.eu>
2019-11-14 11:43:40 +0100

    allowed to specify mappings for segmentation refs #30622

--- converter/__init__.py
+++ converter/__init__.py
@@ -242,10 +242,15 @@ class Converter(object):
         current_directory = os.getcwd()
         os.chdir(working_directory)
         segment_time = max(options.get('segment_time', 1), math.ceil(options.get('audio', {}).get('start_time', 1)))
+
         optlist = [
             "-flags", "-global_header", "-f", "segment", "-segment_time", "%s" % segment_time, "-segment_list", output_file, "-segment_list_type", "m3u8", "-segment_format", "mpegts",
-            "-segment_list_entry_prefix", "%s/" % output_directory, "-map", "0", "-map", "-0:d", "-vcodec", "copy", "-acodec", "copy"
+            "-segment_list_entry_prefix", "%s/" % output_directory
         ]
+        for input_map in (options.get('maps') or ['0']):
+            optlist.extend(['-map', str(input_map)])
+        optlist.extend(["-vcodec", "copy", "-acodec", "copy"])
+
         try:
             if "video" in info.streams[0].type:
                 codec = info.streams[0].codec
