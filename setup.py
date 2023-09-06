56ff386d41ecc05baa720c9ec03d17fe547b72df
Stéphane Diemer <stephane.diemer@ubicast.eu>
2023-09-06 15:53:52 +0200

    Change version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class DocCommand(Command):
 
 setup(
     name='VideoConverter',
-    version='2.0.1',
+    version='2.1.0',
     description='Video Converter library',
     url='https://github.com/senko/python-video-converter/',
 
